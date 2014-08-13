e51277500a40ad6ce7cfba94c17b684e622416e8
Pamela <belulali>
2014-08-13 14:17:31 -0500

    Algorithm that transforms decimal numbers into roman numerals

---
+++ Exercises/RomanNumerals.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercises
+{
+    public class RomanNumerals
+    {
+        private readonly Dictionary<int, string> _romanNumbers = new Dictionary<int, string>()
+            {
+                {1000, "M"}, 
+                {900, "CM"}, 
+                {500, "D"}, 
+                {400, "CD"}, 
+                {100, "C"}, 
+                {90, "CX"}, 
+                {50, "L"}, 
+                {40, "XL"}, 
+                {10, "X"}, 
+                {9, "IX"}, 
+                {5, "V"}, 
+                {4, "IV"},
+                {1, "I"}
+            };
+
+        public string Transform(int number)
+        {
+            // Initialize an empty variable that will contain roman numbers
+            // Determine the closest roman number
+            // Add the roman number to the string
+            // Subtract the value of the roman number from the number
+            // if number zero then exit and return string
+            // Other wise start again
+
+            var result = "";
+
+            while (number != 0)
+            {
+                var closestValue = ClosestValue(number);
+                result += closestValue.Single().Value;
+                number -= closestValue.Single().Key; 
+            }
+            return result;
+            
+        }
+
+        private Dictionary<int, string> ClosestValue(int number)
+        {
+            var romanNumeralsDictonary = new Dictionary<int, string>();
+           
+            foreach (var romanNumber in _romanNumbers.Keys)
+            {
+                if (romanNumber <= number)
+                {
+                    romanNumeralsDictonary.Add(romanNumber, _romanNumbers[romanNumber]);
+                    return romanNumeralsDictonary;
+                }
+
+            }
+            
+            return romanNumeralsDictonary;
+        }
+    }
+}
---
+++ ExercisesTests/ExercisesTests.cs
@@ -0,0 +1,72 @@
+﻿using Exercises;
+using NUnit.Framework;
+
+namespace ExercisesTests
+{
+    [TestFixture]
+    public class RomanNumeralsTests
+    {
+        RomanNumerals _romanNumerals;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _romanNumerals = new RomanNumerals();
+        }
+
+        [Test]
+        public void ShouldReturnIWhenGivenOne()
+        {
+            var transform = _romanNumerals.Transform(1);
+            Assert.That(transform, Is.EqualTo("I"));
+        }
+
+        [Test]
+        public void ShouldReturnIIWhenGivenTwo()
+        {
+            var transform = _romanNumerals.Transform(2);
+            Assert.That(transform, Is.EqualTo("II"));
+        }
+
+        [Test]
+        public void ShouldReturnIIIWhenGivenThree()
+        {
+            var transform = _romanNumerals.Transform(3);
+            Assert.That(transform, Is.EqualTo("III"));
+        }
+
+        [Test]
+        public void ShouldReturnIVWhenGivenFour()
+        {
+            var transform = _romanNumerals.Transform(4);
+            Assert.That(transform, Is.EqualTo("IV"));
+        }
+
+        [Test]
+        public void ShouldReturnVWhenGivenFive()
+        {
+            var transform = _romanNumerals.Transform(5);
+            Assert.That(transform, Is.EqualTo("V"));
+        }
+
+        [Test]
+        public void ShouldReturnLVIIIWhenGivenFiftyEight()
+        {
+            var transform = _romanNumerals.Transform(58);
+            Assert.That(transform, Is.EqualTo("LVIII"));
+        }
+
+        [Test]
+        public void ShouldReturnLCXLIWhenGivenOneHundredFourtyOne()
+        {
+            var transform = _romanNumerals.Transform(141);
+            Assert.That(transform, Is.EqualTo("CXLI"));
+        }
+
+        public void ShouldReturnDCCLXXXIIWhenGivenSevenHundredEightyTwo()
+        {
+            var transform = _romanNumerals.Transform(782);
+            Assert.That(transform, Is.EqualTo("DCCLXXXII"));
+        }
+    }
+}
